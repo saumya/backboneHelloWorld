73ce30d55d1e6f84185317edea8b8c574498b7cd
saumya <saumya>
2013-01-28 15:16:54 +0530

    added the default image

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/*
 (function ($) {
   
   Friend = Backbone.Model.extend({
@@ -38,3 +39,21 @@
   
   var appview = new AppView;
 })(jQuery);
+*/
+(function ($) {
+    var contacts = [
+        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
+    ];
+    var Contact = Backbone.Model.extend({
+				    defaults: {
+				        photo: "/img/placeholder.jpg"
+				    }
+				});
+} (jQuery));
